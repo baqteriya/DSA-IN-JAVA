6b969e66de3373ea0f15c23aade7de3165735ca9
Riya Kumari <kumaririya1030@gmail.com>
2026-06-12 02:08:01 +0530

    Consonants in string

---
+++ Strings/Consonants.java
@@ -0,0 +1,14 @@
+public class Consonants {
+    public static void main(String[] args) {
+        String str ="programming";
+        int count =0;
+        for(int i=0;i<str.length();i++){
+            char ch = str.charAt(i);
+            if(ch != 'a' && ch != 'e' && ch != 'i' && ch != 'o' && ch != 'u'){
+                count++;
+            }
+        }
+            System.out.println("Consonants:"+ count);
+        }
+    }
+
