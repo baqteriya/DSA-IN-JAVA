ce91295001f1ccf3abede323ffbdf2cef36dda5d
Riya Kumari <kumaririya1030@gmail.com>
2026-06-12 01:46:17 +0530

    String used reverse

---
+++ Strings/Reverse.java
@@ -0,0 +1,10 @@
+class Reverse{
+    public static void main(String[] args) {
+        String str ="Pankhuri";
+        String reversed ="";
+        for(int i=str.length()-1;i>=0;i--){
+            reversed += str.charAt(i);
+        }
+        System.out.println(reversed);
+    }
+}
