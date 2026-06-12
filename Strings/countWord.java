1b2ea546fc21cb2d6b8e90ae44e732d76fa4e1f0
Riya Kumari <kumaririya1030@gmail.com>
2026-06-12 12:49:23 +0530

    Countword

---
+++ Strings/countWord.java
@@ -0,0 +1,7 @@
+public class countWord {
+    public static void main(String[] args) {
+        String str ="i love java";
+        String words[] = str.split(" ");
+        System.out.println(words.length);
+    }
+}
