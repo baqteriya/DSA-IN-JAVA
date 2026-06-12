6e5ecae078811e137105b9142a04fb6e203b0ce7
Riya Kumari <kumaririya1030@gmail.com>
2026-06-12 12:57:22 +0530

    RemoveSpace

---
+++ Strings/RemoveSpace.java
@@ -0,0 +1,7 @@
+public class RemoveSpace {
+    public static void main(String[] args) {
+        String str =" i love java";
+        str = str.replace(" ","");
+        System.out.println(str);
+    }
+}
