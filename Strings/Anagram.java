0f541dd1c5155ea13e81f346c281647175a62a69
Riya Kumari <kumaririya1030@gmail.com>
2026-06-12 12:57:47 +0530

    Anagram

---
+++ Strings/Anagram.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+public class Anagram {
+    public static void main(String[] args) {
+        String str1 = "client";
+        String str2 = "silent";
+       char a[] = str1.toCharArray();
+       char b[] = str2.toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
+        if(Arrays.equals(a,b)){
+            System.out.println("anagram");
+        }else{
+            System.out.println("not anagram");
+        }
+
+
+        
+
+    }
+}
