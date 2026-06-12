b977a81e01628f2b545a9b3f7db6911f0906f1a9
Riya Kumari <kumaririya1030@gmail.com>
2026-06-12 13:31:07 +0530

    Average of Array

---
+++ Arrays/AverageArray.java
@@ -0,0 +1,13 @@
+public class AverageArray {
+    public static void main(String[] args) {
+        int arr[]={10,20,30,40,50};
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum = sum + arr[i];
+
+        }
+        int average = sum /arr.length;
+        System.out.println(average);
+    }
+    
+}
