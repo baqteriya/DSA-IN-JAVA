0d1ea9ec30817ec3bfa936c734b4ddd93eb1bbad
Riya Kumari <kumaririya1030@gmail.com>
2026-06-12 14:03:50 +0530

    Array of Sorting

---
+++ Arrays/ArraySorting.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+public class ArraySorting {
+    public static void main(String[] args) {
+        int arr[]={12,2,1,33,6,7};
+          Arrays.sort(arr);
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]+"");
+        }
+      
+    }
+    
+}
