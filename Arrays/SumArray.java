a385b5f916683a0377a95a54c262e80f7f334ca2
Riya Kumari <kumaririya1030@gmail.com>
2026-06-12 13:28:12 +0530

    Sum of Arrays

---
+++ Arrays/SumArray.java
@@ -0,0 +1,10 @@
+public class SumArray {
+    public static void main(String[] args) {
+        int arr[]={11,22,33,44,55};
+        int sum=0;
+        for(int i=0;i <arr.length;i++){
+            sum = sum + arr[i];
+        }
+        System.out.println(sum);
+    }
+}
