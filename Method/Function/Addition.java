8fac61753257cfcd45bb11c35467199e14d260da
Riya Kumari <kumaririya1030@gmail.com>
2026-06-13 11:06:21 +0530

    Add Two No

---
+++ Method/Function/Addition.java
@@ -0,0 +1,10 @@
+public class Addition {
+    public  static  int add(int num1,int num2){
+        int total = num1 + num2;
+        System.out.println(total);
+        return total;
+    }
+    public static void main(String[] args) {
+        add( 4,5);
+    }
+}
