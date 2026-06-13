cbea6789d655c15b1d00aad0c88fb31a684c2dba
Riya Kumari <kumaririya1030@gmail.com>
2026-06-13 11:05:45 +0530

    Hello print

---
+++ Method/Function/PrintMethodHello.java
@@ -0,0 +1,10 @@
+
+public class PrintMethodHello  {
+    public static void printDisplay(){
+        System.out.println("Hello World");
+    }
+    public static void main(String[] args) {
+        printDisplay();
+    }
+    
+}
