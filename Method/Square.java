0bf7eb7d340a0d65dbf82b06d3ebe9f6702e6978
Riya Kumari <kumaririya1030@gmail.com>
2026-06-13 11:07:28 +0530

    square

---
+++ Method/Square.java
@@ -0,0 +1,9 @@
+public class Square {
+    public static void printDisplay(int n){
+        int square = n * n;
+        System.out.println(square);
+    }
+    public static void main(String[] args) {
+        printDisplay(8);
+    }
+}
