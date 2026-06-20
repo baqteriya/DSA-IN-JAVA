9f6fce1c34ef1a293f70d4a3509c61bbd2a1b160
Riya Kumari <kumaririya1030@gmail.com>
2026-06-20 11:35:28 +0530

    unique element

---
+++ HashSet/UniqueElement.java
@@ -0,0 +1,12 @@
+import java.util.HashSet;
+public class UniqueElement {
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6};
+        HashSet <Integer> set = new HashSet<>();
+        for(int num:arr){
+            set.add(num);
+
+        }
+        System.out.println("unique element ="+ set.size());
+    }
+}
