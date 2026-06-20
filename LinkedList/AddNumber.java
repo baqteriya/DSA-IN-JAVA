778cd0109ebc5ef54e628d84c0c862e8ef4698f0
Riya Kumari <kumaririya1030@gmail.com>
2026-06-20 11:00:40 +0530

    sum of element

---
+++ LinkedList/AddNumber.java
@@ -0,0 +1,17 @@
+import java.util.LinkedList;
+public class AddNumber {
+    public static void main(String[] args) {
+        LinkedList <Integer> list = new LinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        int sum =0;
+        for(int i=0;i<list.size();i++){
+            sum = sum + list.get(i);
+        }
+        System.out.println(sum);
+    }
+    
+}
