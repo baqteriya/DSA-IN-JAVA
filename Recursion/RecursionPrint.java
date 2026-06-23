c119cb281c6890ce38527379cf4173119e97c62b
Riya Kumari <kumaririya1030@gmail.com>
2026-06-23 20:33:48 +0530

    print  N to 1

---
+++ Recursion/RecursionPrint.java
@@ -0,0 +1,14 @@
+public class RecursionPrint {
+    public static void printNum(int n){
+        if(n == 0){
+            return ;
+        }
+        System.out.println(n);
+        printNum(n-1);
+
+    }
+   public static void main(String[] args) {
+    printNum(5);
+
+   } 
+}
