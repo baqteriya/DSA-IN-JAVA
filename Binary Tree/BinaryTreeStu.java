0d31e4d597bcc342b72cbb093cc5fa043591e047
Riya Kumari <kumaririya1030@gmail.com>
2026-08-29 15:00:58 +0530

    Create MaxDepth

---
+++ Binary Tree/BinaryTreeStu.java
@@ -0,0 +1,27 @@
+class TreeNode{
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int val){
+        this.val = val;
+    }
+}
+public class BinaryTreeStu {
+    public static int maxDepth(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        return 1 + Math.max(leftDepth,rightDepth);
+    }
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        System.out.println(maxDepth (root));
+    }
+
+    
+}
