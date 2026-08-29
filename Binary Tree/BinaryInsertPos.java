65e4bd1cc6e75d94d9af6b88af2d9d9e07a3c632
Riya Kumari <kumaririya1030@gmail.com>
2026-08-29 14:39:56 +0530

    BinaryTree insert a position

---
+++ Binary Tree/BinaryInsertPos.java
@@ -0,0 +1,20 @@
+public class BinaryInsertPos {
+    public static void main(String[] args) {
+        int[]nums={1,3,5,6};
+        int target =5;
+        int left =0;
+        int right =nums.length-1;
+        while(left<= right){
+            int mid =(left+right)/2;
+            if(nums[mid]<target){
+                left = mid+1;
+            }else if(nums[mid]>target){
+                right =mid-1;
+            }else{
+                System.out.println("Found at index:"+mid);
+                return;
+            }
+        }
+        System.out.println("Found at index:"+left);
+    }
+}
