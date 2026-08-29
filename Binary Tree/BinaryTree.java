44328496e497b973614ef7b416b0cd159a5c7ced
Riya Kumari <kumaririya1030@gmail.com>
2026-08-29 14:33:19 +0530

    Create BinaryTree

---
+++ Binary Tree/BinaryTree.java
@@ -0,0 +1,20 @@
+public class BinaryTree {
+    public static void main(String[] args) {
+        int[]nums={1,3,5,7,9,11,13};
+        int target =9;
+        int left =0;
+        int right =nums.length-1;
+        while(left<= right){
+            int mid =(left + right)/2;
+            if(nums[mid]< target){
+                left = mid+1;
+            }else if(nums[mid]>target){
+                right = mid-1;
+            }else{
+                System.out.println("Found at index:"+ mid);
+                break;
+            }
+        }
+    }
+    
+}
